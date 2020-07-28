8193ac79c61b010896f3afc77e2ec86c441c38fa
mmemmemme <68884989+mmemmemme@users.noreply.github.com>
2020-07-28 21:06:21 +0900

    Add files via upload

--- Script.js
+++ Script.js
@@ -168,7 +168,7 @@ window.onload = function () {
 		tweetBtn.ontouchend = function () {				//S_Tweetボタンをタッチした（タッチして離した）時にこの中の内容を実行する
 			//ツイートＡＰＩに送信
 			//結果ツイート時にURLを貼るため、このゲームのURLをここに記入してURLがツイート画面に反映されるようにエンコードする
-			const url = encodeURI("https://hothukurou.com");
+			const url = encodeURI("https://mmemmemme.github.io/TouchGame/");
 			window.open("http://twitter.com/intent/tweet?text=頑張って" + point + "ねっこを撫でたった!!!!!!!&hashtags=ahoge&url=" + url); //ハッシュタグにahogeタグ付くようにした。
 		};
 
